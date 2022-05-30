903be589cf6f52eabf2f009d50b5e0c96c5e7c17
Josyto <josyto7@gmail.com>
2022-05-30 19:58:00 +0200

    Project initialized

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['standard', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
+};
