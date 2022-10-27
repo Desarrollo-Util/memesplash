75a06c777fb1a5f59076a886b686e2bf054b4a9e
SirReiva <gjjavicoru@hotmail.com>
2022-10-27 14:38:08 +0200

    fix gen user

--- test/utils/generate-random-user.ts
+++ test/utils/generate-random-user.ts
@@ -2,7 +2,7 @@ import { randEmail, randFullName, randPassword, randUuid } from '@ngneat/falso';
 
 export const generateRandomUser = () => ({
     id: randUuid(),
-    name: randFullName(),
+    name: randFullName({ withAccents: false }),
     email: randEmail(),
     password: randPassword({ size: 15 }),
 });
